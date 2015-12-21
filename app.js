59ffe2964f05d7aaded18bf9ec78abfaf200d7fb
Jiivee <joni.vayrynen@gmail.com>
2015-12-21 17:37:07 +0200

    Add match schema

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var app = express();
 
 //var routes = require('./routes/index');
 var users = require('./routes/users');
+var matches = require('./routes/matches');
+var addMatch = require('./routes/addmatch');
 
 //load all files in models dir
 fs.readdirSync(__dirname + '/models').forEach(function(filename){
@@ -27,19 +29,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/users', users);
+app.use('/matches', matches);
+app.use('/addmatch', addMatch);
+
 
 /*
-app.configure('development', function() {
-  mongoose.connect('mongodb://localhost/altruDb', function(err, db) {
-  if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      console.log('Connection established');
-    }
-  });
+mongoose.connect('mongodb://localhost/fv', function(err, db) {
+if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established');
+  }
 });
 */
-
 mongoose.connect('mongodb://heroku_n8tthvx3:gg52807ergarga789k8f20jmp1@ds033285.mongolab.com:33285/heroku_n8tthvx3');
 
 
--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,11 +1,9 @@
-//Gather users collection from the db.
-
 var mongoose = require('mongoose');
 var Schema 	= mongoose.Schema;
 
 var usersSchema = new Schema({
 	name: String,
-	email: String,
+  email: String,
 });
 
 mongoose.model('users', usersSchema);
