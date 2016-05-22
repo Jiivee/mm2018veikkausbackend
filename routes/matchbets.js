992e8eb4fe9e79dd340913532a3db3bc4b46ab42
Jiivee <joni.vayrynen@gmail.com>
2016-05-22 23:48:14 +0300

    make bets works

--- routes/matchbets.js
+++ routes/matchbets.js
@@ -4,17 +4,37 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
-var router = express.Router();
+//var router = express.Router();
 //ONLY Allow access for authenticated user:
 var router = require('../auth.js');
 
 var async = require('async');
 
+var populateQuery = {path: 'match', model: mongoose.model('match')};
+
+var populateTeamsQuery = [
+  {
+    path: 'match.home_team',
+    model: mongoose.model('team')
+  },
+  {
+    path: 'match.away_team',
+    model: mongoose.model('team')
+  }
+];
+
+router.get('/:userId/:tournamentId', function(req, res, next) {
+  mongoose.model('matchbet').find({tournament: req.params.tournamentId, user: req.params.userId}).sort('match').populate(populateQuery).exec(function(err, matchbets) {
+    mongoose.model('match').populate(matchbets, populateTeamsQuery, function(err, matchbets) {
+      res.send(matchbets);
+    })
+  })
+})
 
 router.put('/', function(req, res, next) {
   var bets = req.body;
 
-  console.log(req.headers);
+  console.log(bets);
   async.each(bets, function(bet, callback) {
     mongoose.model('matchbet').findById(bet._id, function(err, matchbet) {
       matchbet.score = bet.score;
@@ -32,6 +52,7 @@ router.put('/', function(req, res, next) {
       });
     });
   });
+  res.send(200);
 });
 
 module.exports = router;
